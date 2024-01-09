4c80695a2b6395fe0ad9cfb4b800d9bac11f24af
bokke1010 <bokke1010@gmail.com>
2024-01-09 15:37:57 +0100

    Add simple movement

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ CELL = {
     "FISH"  : 2
 }
 
+#REMEMBER: 0,0 is top left
 DIRS = {
     "N"  : 0,
     "NE" : 1,
@@ -24,17 +25,17 @@ DIRS = {
 # Direction symbols, including marker
 dir_symbs = ['↑', '↗', '→', '↘', '↓', '↙', '←', '↖', 'o', 'X']
 
-dir_to_offset = [(0,1),(1,1), (1,0), (1,-1), (0,-1), (-1, -1), (-1, 0), (-1, 1), (0,0)]
+dir_to_offset = [(0,-1),(1,-1), (1,0), (1,1), (0,1), (-1, 1), (-1, 0), (-1, -1), (0,0)]
 
 offset_to_dir = {
-    (0,1)   : 0,
-    (1,1)   : 1,
+    (0,-1)   : 0,
+    (1,-1)   : 1,
     (1,0)   : 2,
-    (1,-1)  : 3,
-    (0,-1)  : 4,
-    (-1, -1): 5,
+    (1,1)  : 3,
+    (0,1)  : 4,
+    (-1, 1): 5,
     (-1, 0) : 6,
-    (-1, 1) : 7,
+    (-1, -1) : 7,
     (0,0)   : 8
 }
 
@@ -57,12 +58,11 @@ class Grid():
 
     # grid : [[Grid_cell]]
 
-    def __init__(self, id):
+    def __init__(self):
         self.grid  = [[Grid_cell()] * GRID_X for _ in range(GRID_Y)]
-        self.id = id
 
     def __repr__(self):
-        return f"{self.id}\n" + "\n".join(" ".join(map(repr, line)) for line in self.grid)
+        return "\n".join(" ".join(map(repr, line)) for line in self.grid)
 
     def set_position(self, x, y, value):
         self.grid[y%GRID_Y][x%GRID_X] = value
@@ -113,24 +113,38 @@ def assign_directions(old_grid, new_grid): # alignment rule
                 assign_direction(old_grid, new_grid, x,y)
 
 def move_fish(old_grid, new_grid): # finialize timestep
-    pass
-    #TODO: move fish
+    for y in range(GRID_Y):
+        for x in range(GRID_X):
+            n_cell = new_grid.get_position(x,y)
+            o_cell = old_grid.get_position(x,y)
+            if o_cell.cell_type != CELL["FISH"]:
+                continue
+            offset = dir_to_offset[n_cell.cell_dir]
+            nx, ny = x + offset[0], y + offset[1]
+            m_cell = new_grid.get_position(nx, ny)
+            if m_cell.cell_type != CELL["EMPTY"]:
+                continue
+            new_grid.set_position(x,y, Grid_cell())
+            new_grid.set_position(nx, ny, n_cell)
+
 
 def iterate_grid(grid, steps):
-    new_grid = Grid(1)
+    new_grid = Grid()
     for _ in range(steps):
         new_grid.clear()
         assign_directions(grid, new_grid)
         move_fish(grid, new_grid)
+        print(new_grid)
+        print('---------------------------------------')
         grid, new_grid = new_grid, grid
     return grid
 
 
 
-grid = Grid(0)
+grid = Grid()
 
 grid.populate_grid(FISH_START_COUNT, FISH_START_RADIUS, FISH_START_CHANCE)
-print(grid)
-print('---------------------------')
+# print(grid)
+# print('---------------------------')
 grid = iterate_grid(grid, 5)
-print(grid)
+# print(grid)

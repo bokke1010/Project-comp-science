29ed200d56f2a108523f463e6c1470f070e2867b
bokke1010 <bokke1010@gmail.com>
2024-01-18 13:45:26 +0100

    Improve visualization to support larger grid

--- parameters.py
+++ parameters.py
@@ -1,9 +1,18 @@
 # Simulation options
-TIME_STEPS = 80
-FPS = 5
+TIME_STEPS = 120
 
-GRID_X = 40
-GRID_Y = 40
+GRID_X = 120
+GRID_Y = 120
+
+#Visualization options
+class VisualizationOptions:
+    FPS = 5
+    CELLS_PER_INCH = 5
+    PIXELS_PER_CELL = 5
+    PIXELS_PER_ENTITY = 6 # May be larger than pixels per cell
+    PIXELS_PER_INCH = CELLS_PER_INCH * PIXELS_PER_CELL
+    FILE_NAME = "video"
+    BG_COLOR = "darkblue"
 
 # Only mode 6 is supported
 MODE_4, MODE_6, MODE_8 = 0,1,2
@@ -11,10 +20,10 @@ GRID_MODE = MODE_6
 
 # Initialization settings
 FISH_START_RADIUS = 2
-FISH_START_COUNT = 3
+FISH_START_COUNT = 5
 FISH_START_CHANCE = 0.7
 
-SHARK_START_COUNT = 3
+SHARK_START_COUNT = 7
 
 # Fish settings
 FISH_SPEED = 1
--- visualize.py
+++ visualize.py
@@ -1,35 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
-from parameters import FPS
+from parameters import VisualizationOptions
 
 writer = None
 circle_grid, colormap = None, None
 
-cell_colors = ["", "black", "yellow", "red", "green"]
+cell_colors = ["", VisualizationOptions.BG_COLOR, "yellow", "red", "white"]
 
 def init(xsize, ysize, mode):
     global writer, savefunc, circle_grid, colormap
     FFMpegWriter = manimation.writers['ffmpeg']
     metadata = dict(title='Movie Test', artist='Matplotlib',
                     comment='a visualization')
-    writer = FFMpegWriter(fps=FPS, metadata=metadata)
+    writer = FFMpegWriter(fps=VisualizationOptions.FPS, metadata=metadata)
 
-    fig = plt.figure(facecolor='black', frameon=False, figsize=((xsize + 0.5) / 5, ysize / 5))
+    fig = plt.figure(
+        frameon=False,
+        figsize=(
+            (xsize + 0.5) / VisualizationOptions.CELLS_PER_INCH,
+            ysize / VisualizationOptions.CELLS_PER_INCH)
+    )
     fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
     ax = fig.add_subplot(111)
     ax.set_xlim(0, xsize + 0.5)
     ax.set_ylim(0, ysize)
-    ax.set_facecolor("black")
+    ax.set_facecolor(VisualizationOptions.BG_COLOR)
 
     x, y = np.arange(0, xsize), np.arange(0, ysize)
     xv, yv = np.meshgrid(x, y)
     xv = xv + (yv % 2) / 2 + 0.5
     yv = yv + 0.5
-    colormap = ["black"] * (xsize * ysize)
+    colormap = [VisualizationOptions.BG_COLOR] * (xsize * ysize)
 
-    circle_grid = ax.scatter(xv, yv, s=40, marker='o', c=colormap)
-    writer.setup(fig, "writer_test.mp4", 100)
+    circle_grid = ax.scatter(xv, yv, s= (2 * VisualizationOptions.PIXELS_PER_ENTITY)**2, marker='o', c=colormap)
+    writer.setup(fig, f"{VisualizationOptions.FILE_NAME}.mp4", VisualizationOptions.PIXELS_PER_INCH)
 
 def visualize(grid):
     i=0

ed2b93c86006a15d19fd1303284452931a97214e
bokke1010 <bokke1010@gmail.com>
2024-01-29 15:02:09 +0100

    Format PerlinNoise.py

--- PerlinNoise.py
+++ PerlinNoise.py
@@ -1,19 +1,26 @@
-import numpy as np 
+# Project Computer Science - PerlinNoise.py
+# This file generates 2D perlin noise.
+# It is used by main.py to generate obstacles.
+
+import numpy as np
+
 
 def smoothstep(t):
-   
+    """Interpolate between 0 and 1 such that the derivative at the endpoints is 0."""
     return t * t * (3 - 2 * t)
 
-def lerp(a, b, t):
 
+def lerp(a, b, t):
+    """Linear interpolation between [a] and [b]."""
     return a + t * (b - a)
 
 
 def generate_perlin_noise(width, height, scale):
+    """Generate a numpy array containing perlin noise."""
 
     noise = np.zeros((height, width))
 
-    gradients = np.random.randn(height // scale + 2, width // scale + 2, 2) 
+    gradients = np.random.randn(height // scale + 2, width // scale + 2, 2)
 
     for y in range(height):
         for x in range(width):
@@ -21,31 +28,39 @@ def generate_perlin_noise(width, height, scale):
             y_cell = y // scale
 
             in_x_cell = x / scale - x_cell
-            in_y_cell = y / scale - y_cell 
+            in_y_cell = y / scale - y_cell
 
             # calculate the dot products
-            dot_top_left = np.dot(gradients[y_cell, x_cell], [in_x_cell, in_y_cell])
-            dot_top_right = np.dot(gradients[y_cell, x_cell + 1], [in_x_cell - 1, in_y_cell])
-            dot_bottom_left = np.dot(gradients[y_cell + 1, x_cell], [in_x_cell, in_y_cell - 1])
-            dot_bottom_right = np.dot(gradients[y_cell + 1, x_cell + 1], [in_x_cell - 1, in_y_cell - 1])
+            dot_top_left = np.dot(gradients[y_cell, x_cell], [
+                                  in_x_cell, in_y_cell])
+            dot_top_right = np.dot(
+                gradients[y_cell, x_cell + 1], [in_x_cell - 1, in_y_cell])
+            dot_bottom_left = np.dot(
+                gradients[y_cell + 1, x_cell], [in_x_cell, in_y_cell - 1])
+            dot_bottom_right = np.dot(
+                gradients[y_cell + 1, x_cell + 1], [in_x_cell - 1, in_y_cell - 1])
 
             # interpolating along the x axis
             interpolate_x = smoothstep(in_x_cell)
 
             interpolated_top = lerp(dot_top_left, dot_top_right, interpolate_x)
-            interpolated_bottom = lerp(dot_bottom_left, dot_bottom_right, interpolate_x)
+            interpolated_bottom = lerp(
+                dot_bottom_left, dot_bottom_right, interpolate_x)
 
             # interpolating along y axis
             interpolate_y = smoothstep(in_y_cell)
 
             # interpolating the final values
-            noise[y, x] = lerp(interpolated_top, interpolated_bottom, interpolate_y)
+            noise[y, x] = lerp(
+                interpolated_top, interpolated_bottom, interpolate_y)
 
-    # normalizing 
+    # normalizing
     noise = (noise - np.min(noise)) / (np.max(noise) - np.min(noise))
 
     return noise
 
+
+# Some demo code to visualize the functionality of this file.
 if __name__ == "__main__":
     width, height = 256, 256
     scale = 50
@@ -56,11 +71,3 @@ if __name__ == "__main__":
     plt.imshow(perlin_noise, interpolation='nearest', cmap="gray")
     plt.colorbar()
     plt.show()
-
-
-
-
-
-
-
-
